782c8fba7e4227eb2f9938d5cf50247dfccaf708
amirkh75 <amirp.1375@gmail.com>
2021-03-04 00:28:46 +0330

    Log in with Token

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 from profiles_api import serializers
@@ -93,7 +95,6 @@ class HelloViewset(viewsets.ViewSet):
 
     def partial_update(self, request, pk=None):
         """Handle updating part of an object"""
-
         return Response({'http_method': 'PATCH'})
 
     def destroy(self, request, pk=None):
@@ -109,3 +110,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfi,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
